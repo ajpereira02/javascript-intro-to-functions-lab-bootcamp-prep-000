e78531fee2c5d38f9f7c1277c02a60fd580750c3
Ashley Pereira <ashleypereira70@gmail.com>
2018-09-14 11:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-return('shout(string)', function() {
-  it('sayHiToGrandma'.toLowerCase(), function() {
-    
-  })
-})
+function shout(string){
+return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log (string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log (string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === whisper(string)){
+      return "I can't hear you!"
+      
+  } else if (string === shout(string)) {
+      return "YES INDEED!"
+      
+    } else if (string === "I love you, Grandma."){
+        return "I love you, too."
+      }
+}
